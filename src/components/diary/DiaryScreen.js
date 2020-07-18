49b99e3967e78b99efccfced5f8f64a222080705
Nik Adzic <nik@adzic.si>
2020-07-18 12:41:20 +0200

    add some basic rendering of diaries

--- src/components/diary/DiaryScreen.js
+++ src/components/diary/DiaryScreen.js
@@ -1,15 +1,67 @@
-import * as React from 'react';
-import { View, Text, TouchableOpacity } from 'react-native';
+import React, { useState, useEffect } from 'react';
+import { View, Text, TouchableOpacity, ImageBackground, StyleSheet, ScrollView, Image } from 'react-native';
+import firestore from '@react-native-firebase/firestore';
 
 export const DiaryScreen = ({ navigation }) => {
+
+  const [diaries, setDiaries] = useState([]);
+  const [error, setError] = useState();
+
+  useEffect(() => {
+    firestore().collection('diaries').get()
+      .then(response => {
+        const fetchedDiaries = [];
+        response.docs.forEach(document => {
+          const fetchedDiary = {
+            id: document.id,
+            ...document.data()
+          };
+          fetchedDiaries.push(fetchedDiary);
+        });
+        setDiaries(fetchedDiaries);
+      })
+      .catch(error => {
+        setError(error);
+      });
+  }, []);
+
   return (
-    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-      <Text>Home Screen</Text>
-      <TouchableOpacity
-        onPress={() => {}}
+      <ImageBackground
+        style={styles.backgroundImage}
+        source={require('DrawApp/assets/png/bg.png')}
       >
-        <Text>Test</Text>
-      </TouchableOpacity>
-    </View>
+      <ScrollView contentContainerStyle={styles.contentContainer}>
+      <Text style={{ color: 'white', marginTop: 70, fontSize: 50, textAlign: 'center' }}>Diaries</Text>
+      {error && <Text>Oops there is an error</Text>}
+      {diaries && diaries.map(diary => (
+        <View style={{ marginTop: 20 }}>
+          <Text style={{ color: 'white' }}>{diary.username}</Text>
+          <Image
+          style={styles.logo}
+          source={{
+            uri: diary.imageUrl
+          }}
+        />
+        </View>
+
+      ))}
+      </ScrollView>
+      </ImageBackground>
   );
 };
+
+
+const styles = StyleSheet.create({
+  contentContainer: {
+    flex: 1,
+  },
+  logo: {
+    width: 300,
+    height: 300,
+  },
+  backgroundImage: {
+    flex: 1,
+    alignItems: 'center',
+    width: '100%',
+  },
+});
